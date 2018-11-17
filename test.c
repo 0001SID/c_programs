4ee7ea1a36429cc15fa0abe52ea919d6c27dc0f1
0001SID <mailofsumandas@gmail.com>
2018-11-17 12:01:04 +0530

    update

--- test.c
+++ test.c
@@ -2,20 +2,7 @@
 #include<stdlib.h>
 int main()
 {
-    FILE *fp; char c;
-    fp = fopen("file.txt","w");
-    fprintf(fp,"This is the new content");
-    fclose(fp);
-    fp = fopen("file.txt","r");
-    while (1)
-    {
-        c = fgetc(fp);
-        if(c == EOF)
-        {
-            break;
-        }
-        printf("%c",c);
-    }
-    fclose(fp);
+    int a = 2;
+    printf("%04d",a);
     return 0;
 }
